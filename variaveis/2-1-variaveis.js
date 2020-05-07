a0d5bfa2ed0153e4e531b58f11c25e545447ed33
Paulo Antonio <paulo.werder@gmail.com>
2020-05-07 18:01:34 +0000

    ajustado

--- variaveis/2-1-variaveis.js
+++ variaveis/2-1-variaveis.js
@@ -8,5 +8,5 @@ var test = 'example';
         console.log(`Valor dentro do if. "${test}"`);
     }
 
-    console.log(`Valor apósa a execução do if "${test}"`);
+    console.log(`Valor após a execução do if "${test}"`);
 })();
